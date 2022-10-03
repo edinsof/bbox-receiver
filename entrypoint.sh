2f01bcc58302dedff193da5060b733326335b375
datagutt <datagutt@lekanger.no>
2022-10-03 17:50:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 export USER=$(whoami)
 
 chmod o+w /dev/stdout
-sudo -E /usr/bin/supervisord --nodaemon --configuration /etc/supervisord.conf
+/usr/bin/supervisord --nodaemon --configuration /etc/supervisord.conf
